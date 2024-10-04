ff2da031f47b7dadc1c531b7fe9ac4066347a934
Jerome Sipin <sipinjerome2000@gmail.com>
2024-10-04 10:19:55 -0400

    added relationoperatorsdemo

---
+++ Module-303/Module-303/src/Homework/M303_2_1/RelationalOperatorsDemo.java
@@ -0,0 +1,21 @@
+package Homework.M303_2_1;
+
+public class RelationalOperatorsDemo {
+    public static void main(String[] args) {
+
+        int x = 10, y = 5;
+        System.out.println("x > y: " + (x > y));
+        System.out.println("x < y: " + (x < y));
+        System.out.println("x >= y: " + (x >= y));
+        System.out.println("x <= y: " + (x <= y));
+        System.out.println("x == y: " + (x == y));
+        System.out.println("x != y: " + (x != y));
+
+        int v1 = 50, v2 = 100, v3 = 50;
+        System.out.println("variable 1 = " + v1);
+        System.out.println("variable 2 = " + v2);
+        System.out.println("variable 3 = " + v3);
+        System.out.println("variable 1 == variable 2: " + (v1 == v2));
+        System.out.println("variable 1 == variable 3: " + (v1 == v3));
+    }
+}
