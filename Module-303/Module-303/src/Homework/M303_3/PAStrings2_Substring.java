277dd365171652bb8c5c6dae736def7dd75800ae
Jerome Sipin <sipinjerome2000@gmail.com>
2024-10-06 13:12:40 -0400

    completed "substring", "substringcomarisons" hackerranks

--- Module-303/Module-303/src/Homework/M303_3/PAStrings2_Substring.java
+++ Module-303/Module-303/src/Homework/M303_3/PAStrings2_Substring.java
@@ -1,4 +1,28 @@
 package Homework.M303_3;
 
+import java.util.Scanner;
+
 public class PAStrings2_Substring {
+    public static void main(String[] args) {
+
+        // Given a string s and two indices, start and end, print a substring consisting of all
+        // characters in the inclusive range from start to end - 1. You'll find the String class'
+        // substring method helpful in completing this challenge.
+
+        // Create scanner, ask for string and start/end indices
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s = scanner.nextLine();
+        System.out.print("Enter starting index: ");
+        int start = scanner.nextInt();
+        System.out.print("Enter ending index: ");
+        int end = scanner.nextInt();
+
+        // Create substring from user's inputs
+        String sSubstring = s.substring(start, end);
+
+        // Print created substring to console
+        System.out.println(sSubstring);
+
+    }
 }
---
+++ Module-303/Module-303/src/Homework/M303_3/PAStrings3_SubstringComparisons.java
@@ -0,0 +1,57 @@
+package Homework.M303_3;
+
+import java.util.Scanner;
+
+public class PAStrings3_SubstringComparisons {
+    public static void main(String[] args) {
+
+        //
+        String smallest = "";
+        String largest = "";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s1 = scanner.nextLine();
+        System.out.print("Enter substring length: ");
+        int x = scanner.nextInt();
+
+        // Set smallest/largest to first substring in selected word
+        smallest = s1.substring(0, x);
+        largest = s1.substring(0, x);
+
+        // For loop through the word. Take substrings of length x, starting at whatever character we are at, and ending
+        // at x + 1 characters before the word ends. For example, in hackerrank, "welcometojava" is split into
+        // wel
+        // elc
+        // lco
+        // com
+        // ome
+        // met
+        // eto
+        // toj
+        // oja
+        // jav
+        // ava
+        // welcometojava
+        // 0123456789012
+
+        int y = (s1.length() - x) + 1; // ends search for substrings x + 1 indices before the word ends.
+        String comparedString; // the string we will be comparing this iteration of the loop
+        int cValueS; // compareTo smallest
+        int cValueL; // compareTo largest
+        for (int i = 0; i < y; i++) {
+            comparedString = s1.substring(i, i + x);
+            cValueS = comparedString.compareTo(smallest);
+            cValueL = comparedString.compareTo(largest);
+            if (cValueS < 0) {
+                smallest = comparedString;
+            }
+            else if (cValueL > 0) {
+                largest = comparedString;
+            }
+        }
+        // return smallest/largest
+        System.out.println("Smallest string: " + smallest);
+        System.out.println("Largest string: " + largest);
+
+    }
+}
