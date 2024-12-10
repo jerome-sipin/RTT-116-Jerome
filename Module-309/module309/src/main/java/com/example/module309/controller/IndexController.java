982a601c682314baf5a8acd5a6a4875d1cf10956
Jerome Sipin <sipinjerome2000@gmail.com>
2024-12-10 16:32:53 -0500

    spring start

---
+++ Module-309/module309/src/main/java/com/example/module309/controller/IndexController.java
@@ -0,0 +1,23 @@
+package com.example.module309.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/index")
+    public ModelAndView index(){
+
+        ModelAndView response = new ModelAndView();
+
+        // this is our index.jsp file (under webapp/web-inf/jsp/index.jsp)
+        response.setViewName("index");
+
+
+
+        return response;
+    }
+
+}
